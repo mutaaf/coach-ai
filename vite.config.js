45387b622afaf5bfa398be10db11c8427b719f4c
mutaaf <mutaafaziz@gmail.com>
2024-12-11 15:51:59 -0600

    fix: simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'mui-vendor': ['@mui/material', '@mui/icons-material'],
-          'mantine-vendor': ['@mantine/core', '@mantine/hooks', '@mantine/form']
-        }
-      }
-    }
+    outDir: 'dist'
   },
   server: {
-    port: 5173,
-    strictPort: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+    port: 5173
   }
 })
